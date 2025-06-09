100b84a71d500ef7791fdab16e3f3bf280351933
fengx <fengx.work@foxmail.com>
2025-06-09 22:42:10 +0800

    deploy

---
+++ baiduStatics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?d2040d6d23d2430bd3d998faa5ef8bca";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
